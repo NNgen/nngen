d6a224a67361f7723c10a4b953144b993389d702
shtaxxx <shta.ky1018@gmail.com>
2019-12-24 15:47:31 +0900

    onnx_matrix_conv2d_transpose_linear

---
+++ tests/onnx_matrix_conv2d_transpose_linear/test_onnx_matrix_conv2d_linear_int32_3x3_stride1_3x3_stride1.py
@@ -0,0 +1,78 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+
+# the next line can be removed after installation
+sys.path.insert(0, os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__)))))
+
+import nngen as ng
+import veriloggen
+
+import onnx_matrix_conv2d_transpose_linear
+
+
+act_shape = (1, 4, 4, 3)
+weight0_shape = (9, 3, 3, 3)
+weight1_shape = (10, 4 * 9)
+act_dtype = ng.int32
+weight_dtype = ng.int32
+stride0 = 1
+padding0 = 0
+with_batchnorm0 = False
+with_batchnorm1 = False
+act_func0 = 'relu'
+act_func1 = 'relu'
+disable_fusion = False
+par_ich = 1
+par_och = 1
+par_col = 1
+par_row = 1
+concur_och = None
+stationary = 'filter'
+chunk_size = 64
+axi_datawidth = 32
+
+
+def test(request, silent=True):
+    veriloggen.reset()
+
+    simtype = request.config.getoption('--sim')
+
+    rslt = onnx_matrix_conv2d_transpose_linear.run(act_shape,
+                                                   weight0_shape, weight1_shape,
+                                                   act_dtype, weight_dtype,
+                                                   stride0,
+                                                   padding0,
+                                                   with_batchnorm0, with_batchnorm1,
+                                                   act_func0, act_func1,
+                                                   disable_fusion,
+                                                   par_ich, par_och, par_col, par_row,
+                                                   concur_och, stationary,
+                                                   chunk_size,
+                                                   axi_datawidth, silent,
+                                                   filename=None, simtype=simtype,
+                                                   outputfile=os.path.splitext(os.path.basename(__file__))[0] + '.out')
+
+    verify_rslt = rslt.splitlines()[-1]
+    assert(verify_rslt == '# verify: PASSED')
+
+
+if __name__ == '__main__':
+    rslt = onnx_matrix_conv2d_transpose_linear.run(act_shape,
+                                                   weight0_shape, weight1_shape,
+                                                   act_dtype, weight_dtype,
+                                                   stride0,
+                                                   padding0,
+                                                   with_batchnorm0, with_batchnorm1,
+                                                   act_func0, act_func1,
+                                                   disable_fusion,
+                                                   par_ich, par_och, par_col, par_row,
+                                                   concur_och, stationary,
+                                                   chunk_size,
+                                                   axi_datawidth, silent=False,
+                                                   filename='tmp.v',
+                                                   outputfile=os.path.splitext(os.path.basename(__file__))[0] + '.out')
+    print(rslt)
