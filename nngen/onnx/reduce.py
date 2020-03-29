27cfeafc025cf8212cdcb0f984b33c328acce223
shtaxxx <shta.ky1018@gmail.com>
2020-03-29 11:57:31 +0900

    onnx.ReduceSum

---
+++ nngen/onnx/reduce.py
@@ -0,0 +1,78 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import collections
+import numpy as np
+
+import nngen.storage as storage
+import nngen.operator as operator
+
+from . import util
+
+
+def _reduce(method, visitor, node, np_method=None):
+
+    args = []
+
+    for src in node.input:
+        src_obj = visitor.visit(src)
+        args.append(src_obj)
+
+    input = args[0]
+    if isinstance(input, (tuple, list)):
+        input = np.array(input)
+
+    shape = input.shape
+
+    keepdims = False
+    axes = None
+
+    for attribute in node.attribute:
+        if attribute.name == 'axes':
+            axes = [v for v in attribute.ints]
+
+        elif attribute.name == 'keepdims':
+            keepdims = attribute.i != 0
+
+    if axes == [i for i in range(len(shape))]:
+        axes = None
+
+    opt_args = [util.optimize_to_raw_value(arg) for arg in args]
+
+    all_ndarray = True
+    for arg in opt_args:
+        if not isinstance(arg, np.ndarray):
+            all_ndarray = False
+            break
+
+    if all_ndarray and np_method is not None:
+        return np_method(*opt_args, axis=axes, keepdims=keepdims)
+
+    kwargs = collections.OrderedDict()
+
+    name = util.get_name(node)
+    kwargs['name'] = name
+
+    kwargs['axis'] = axes
+    kwargs['keep_dims'] = keepdims
+
+    if name in visitor.value_dtypes:
+        kwargs['dtype'] = visitor.value_dtypes[name]
+
+    return method(*args, **kwargs)
+
+
+def ReduceSum(visitor, node):
+
+    return _reduce(operator.reduce_sum, visitor, node, np.sum)
+
+
+def ReduceMax(visitor, node):
+
+    return _reduce(operator.reduce_max, visitor, node, np.max)
+
+
+def ReduceMin(visitor, node):
+
+    return _reduce(operator.reduce_min, visitor, node, np.min)
